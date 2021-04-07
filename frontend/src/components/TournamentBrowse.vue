7dadeacee9815525795410944c92ada6a5360e10
Jacob Burton <jmburton13@gmail.com>
2021-04-06 21:26:13 -0400

    placeholder implementation of tournamentcard component, tournamentbrowse view, and tournamentservice

---
+++ frontend/src/components/TournamentBrowse.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="card m-3">
+        <img class="card-img-top" v-bind:src="tournament.image"/>
+        <div class="card-body">
+            <h5 class="card-title">{{ tournament.name }}</h5>
+            <p class="card-text">Start Date: {{ tournament.start_date }}</p>
+            <p class="card-text">{{ tournament.description }}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ["tournament"]
+    
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ frontend/src/views/TournamentBrowse.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <div v-for="tournament in tournaments" :key="tournament.tournament_id" :tournament="tournament">
+            <tourney-card :>
+        </div>
+    </div>
+</template>
+
+<script>
+import TournamentCard from '../components/TournamentCard.vue'
+export default {
+  components: { TournamentCard },
+    
+}
+</script>
+
+<style scoped>
+
+</style>
