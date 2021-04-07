01c6ed478fef05d97e770d4434e6ae256cd9783d
Mike Vay <mikevay@gmail.com>
2021-04-07 14:45:29 -0400

    small changes based on conversation with Howie. Added tournament start time and made match start dta and start time nullable

--- backend/database/newschema.sql
+++ backend/database/newschema.sql
@@ -85,6 +85,7 @@ CREATE TABLE tournament (
     max_teams integer NOT NULL,
     elimination_type varchar(50) NOT NULL,
     start_date date NOT NULL,
+    start_time time NOT NULL,
     end_date date NOT NULL,
     signup_open date NOT NULL,
     signup_close date NOT NULL,
@@ -115,8 +116,8 @@ CREATE TABLE matches (
     losing_team_id integer,
     winning_team_score integer,
     losing_team_score integer,
-    match_date date NOT NULL,
-    match_time time NOT NULL,
+    match_date date,
+    match_time time,
 
     CONSTRAINT FK_tournament_id FOREIGN KEY (tournament_id) REFERENCES tournament (tournament_id)
 );
@@ -145,17 +146,17 @@ INSERT INTO team_name_users (user_id, team_id) VALUES (3, 1), (8, 1), (7, 1),
 (7, 5), (6, 5);
 
 INSERT INTO tournament (tournament_id, game_type_id, host_id, name, description, is_open, is_private, 
-max_teams, elimination_type, start_date, end_date, signup_open, signup_close) VALUES 
+max_teams, elimination_type, start_date, end_date, signup_open, signup_close, start_time) VALUES 
 (1, 1, 3, 'Golf Extravaganza', 'Come play in this super cool golf tournament!', true, false, 8, 'Single', 
-'05/01/2021', '05/05/2021', '04/20/2021', '04/28/2021'), 
+'05/01/2021', '05/05/2021', '04/20/2021', '04/28/2021', '12:00:00'), 
 (2, 3, 4, 'Hockey Hooplah', 'Hittin up the hockey gods for a fun tournament - INVITE ONLY - ', true, true, 4, 'Single', 
-'06/01/2021', '06/05/2021', '05/20/2021', '05/28/2021'),
+'06/01/2021', '06/05/2021', '05/20/2021', '05/28/2021', '12:00:00'),
 (3, 5, 5, 'Table Tennis Tourney', 'Get it on with some ping pong!', false, false, 4, 'Single', 
-'03/01/2021', '03/05/2021', '02/20/2021', '02/28/2021'),
+'03/01/2021', '03/05/2021', '02/20/2021', '02/28/2021', '12:00:00'),
 (4, 8, 6, 'PUBG Chicken Dinner Winner', 'Wanna win this dinner? You better be the winner!', true, false, 6, 'Single', 
-'04/15/2021', '05/01/2021', '04/01/2021', '04/14/2021'),
+'04/15/2021', '05/01/2021', '04/01/2021', '04/14/2021', '12:00:00'),
 (5, 14, 7, 'Viscious Valorant', 'Victory to the Valorant Victor who Visciously Vanquishes their foes', true, false, 8, 'Single', 
-'05/01/2021', '05/05/2021', '04/01/2021', '04/28/2021');
+'05/01/2021', '05/05/2021', '04/01/2021', '04/28/2021', '12:00:00');
 
 INSERT INTO tournament_users (tournament_id, team_id, user_id) VALUES (1, 1, 3), (1, 2, 4), (1, 3, 5), 
 (2, 4, 5), (2, 5, 7), 
