aa4e0d24b5b6053d9a0faee1574abc897c5d649d
Mike Vay <mikevay@gmail.com>
2021-04-08 15:55:29 -0400

    fixed integration test for tournament and fixed database script to avoid testing issues

--- backend/database/newschema.sql
+++ backend/database/newschema.sql
@@ -122,22 +122,22 @@ CREATE TABLE matches (
     CONSTRAINT FK_tournament_id FOREIGN KEY (tournament_id) REFERENCES tournament (tournament_id)
 );
 
-INSERT INTO users (user_id,username,password_hash,role) VALUES (1, 'user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
-INSERT INTO users (user_id,username,password_hash,role) VALUES (2, 'admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
-INSERT INTO users (user_id,username,password_hash,role) VALUES (3, 'MikeMan','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
-INSERT INTO users (user_id,username,password_hash,role) VALUES (4, 'KorryTheCowboy','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
-INSERT INTO users (user_id,username,password_hash,role) VALUES (5, 'JacobyBurtonby','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
-INSERT INTO users (user_id,username,password_hash,role) VALUES (6, 'HowieTheNinja','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
-INSERT INTO users (user_id,username,password_hash,role) VALUES (7, 'StephenTheGreat','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
-INSERT INTO users (user_id,username,password_hash,role) VALUES (8, 'TomMasterOfScrum','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
+INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
+INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
+INSERT INTO users (username,password_hash,role) VALUES ('MikeMan','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
+INSERT INTO users (username,password_hash,role) VALUES ('KorryTheCowboy','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
+INSERT INTO users (username,password_hash,role) VALUES ('JacobyBurtonby','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
+INSERT INTO users (username,password_hash,role) VALUES ('HowieTheNinja','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
+INSERT INTO users (username,password_hash,role) VALUES ('StephenTheGreat','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
+INSERT INTO users (username,password_hash,role) VALUES ('TomMasterOfScrum','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
 
 INSERT INTO game_type (game_type_id, game_type) VALUES (1, 'Golf'), (2, 'Baseball'), (3, 'Hockey'), 
 (4, 'Basketball'), (5, 'Table Tennis'), (6, 'Tennis'), (7, 'Football'), (8, 'PUBG'), (9, 'Fortnite'), 
 (10, 'Apex Legends'), (11, 'Leage of Legends'), (12, 'Counter-Strike'), (13, 'Hearthstone'), 
 (14, 'Valorant'), (15, 'Rainbow Six Siege');
 
-INSERT INTO team_name (team_id, team_name) VALUES (1, 'Team One With All'), (2, 'Two True for You'), 
-(3, 'Team Three for Me'), (4, 'Team Four on the Floor'), (5, 'Team Five Guys and Fries');
+INSERT INTO team_name (team_name) VALUES ('Team One With All'), ('Two True for You'), 
+('Team Three for Me'), ('Team Four on the Floor'), ('Team Five Guys and Fries');
 
 INSERT INTO team_name_users (user_id, team_id) VALUES (3, 1), (8, 1), (7, 1), 
 (4, 2), (3, 2), (8, 2), 
@@ -145,17 +145,17 @@ INSERT INTO team_name_users (user_id, team_id) VALUES (3, 1), (8, 1), (7, 1),
 (5, 4), (6, 4), 
 (7, 5), (6, 5);
 
-INSERT INTO tournament (tournament_id, game_type_id, host_id, name, description, is_open, is_private, 
+INSERT INTO tournament (game_type_id, host_id, name, description, is_open, is_private, 
 max_teams, elimination_type, start_date, end_date, signup_open, signup_close, start_time) VALUES 
-(1, 1, 3, 'Golf Extravaganza', 'Come play in this super cool golf tournament!', true, false, 8, 'Single', 
+(1, 3, 'Golf Extravaganza', 'Come play in this super cool golf tournament!', true, false, 8, 'Single', 
 '2021-05-01', '2021-05-05', '2021-04-20', '2021-04-28', '12:00'), 
-(2, 3, 4, 'Hockey Hooplah', 'Hittin up the hockey gods for a fun tournament - INVITE ONLY - ', true, true, 4, 'Single', 
+(3, 4, 'Hockey Hooplah', 'Hittin up the hockey gods for a fun tournament - INVITE ONLY - ', true, true, 4, 'Single', 
 '2021-06-01', '2021-06-05', '2021-05-20', '2021-05-28', '11:00'),
-(3, 5, 5, 'Table Tennis Tourney', 'Get it on with some ping pong!', false, false, 4, 'Single', 
+(5, 5, 'Table Tennis Tourney', 'Get it on with some ping pong!', false, false, 4, 'Single', 
 '2021-03-01', '2021-03-05', '2021-02-20', '2021-02-28', '17:00'),
-(4, 8, 6, 'PUBG Chicken Dinner Winner', 'Wanna win this dinner? You better be the winner!', true, false, 6, 'Single', 
+(8, 6, 'PUBG Chicken Dinner Winner', 'Wanna win this dinner? You better be the winner!', true, false, 6, 'Single', 
 '2021-04-15', '2021-05-01', '2021-04-01', '2021-04-14', '09:00'),
-(5, 14, 7, 'Viscious Valorant', 'Victory to the Valorant Victor who Visciously Vanquishes their foes', true, false, 8, 'Single', 
+(14, 7, 'Viscious Valorant', 'Victory to the Valorant Victor who Visciously Vanquishes their foes', true, false, 8, 'Single', 
 '2021-05-01', '2021-05-05', '2021-04-01', '2021-04-28', '13:00');
 
 INSERT INTO tournament_users (tournament_id, team_id, user_id) VALUES (1, 1, 3), (1, 2, 4), (1, 3, 5), 
@@ -164,10 +164,10 @@ INSERT INTO tournament_users (tournament_id, team_id, user_id) VALUES (1, 1, 3),
 (4, 5, 7), (4, 1, 8), (4, 2, 3), (4, 3, 5), 
 (5, 1, 7), (5, 2, 8), (5, 4,5 ), (5, 5, 6);
 
-INSERT INTO matches (match_id, tournament_id, team_1_id, team_2_id, match_date, match_time, winning_team_id, 
+INSERT INTO matches (tournament_id, team_1_id, team_2_id, match_date, match_time, winning_team_id, 
 losing_team_id, winning_team_score, losing_team_score) VALUES 
-(1, 3, 1, 2, '2021-03-01', '12:00', 1, 2, 5, 3), 
-(2, 3, 3, 4, '2021-03-02', '12:00', 3, 4, 7, 5), 
-(3, 3, 1, 3, '2021-03-04', '14:00', 1, 3, 6, 4);
+(3, 1, 2, '2021-03-01', '12:00', 1, 2, 5, 3), 
+(3, 3, 4, '2021-03-02', '12:00', 3, 4, 7, 5), 
+(3, 1, 3, '2021-03-04', '14:00', 1, 3, 6, 4);
 
 COMMIT TRANSACTION;
