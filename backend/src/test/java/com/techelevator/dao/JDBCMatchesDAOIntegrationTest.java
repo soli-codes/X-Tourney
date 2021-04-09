58af7df146bd9404a082703d5176616e0d37f4f3
Mike Vay <mikevay@gmail.com>
2021-04-08 23:04:36 -0400

    started matches integration tests and completed user integration tests

---
+++ backend/src/test/java/com/techelevator/dao/JDBCMatchesDAOIntegrationTest.java
@@ -0,0 +1,124 @@
+package com.techelevator.dao;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import com.techelevator.model.Matches;
+
+public class JDBCMatchesDAOIntegrationTest extends DAOIntegrationTest {
+	
+	private MatchesDAO matchesDao;
+	private JdbcTemplate jdbcTemplate;
+	private Matches matchesOne;
+	private Matches matchesTwo;
+	private Matches matchesThree;
+
+	@Before
+	public void setup() {
+		jdbcTemplate = new JdbcTemplate(dataSource);
+		matchesDao = new JDBCMatchesDAO(jdbcTemplate);
+		
+		matchesOne = new Matches();
+		matchesTwo = new Matches();
+		matchesThree = new Matches();
+		
+		matchesOne.setMatchId(1000);
+		matchesOne.setTournamentId(1);
+		matchesOne.setTeamOneId(1);
+		matchesOne.setTeamTwoId(2);
+		matchesOne.setWinningTeamId(1);
+		matchesOne.setLosingTeamId(2);
+		matchesOne.setWinningTeamScore(5);
+		matchesOne.setLosingTeamScore(4);
+		matchesOne.setMatchDate("2021-01-01");
+		matchesOne.setMatchTime("12:00");
+		
+		matchesTwo.setMatchId(2000);
+		matchesTwo.setTournamentId(2);
+		matchesTwo.setTeamOneId(2);
+		matchesTwo.setTeamTwoId(3);
+		matchesTwo.setWinningTeamId(2);
+		matchesTwo.setLosingTeamId(3);
+		matchesTwo.setWinningTeamScore(5);
+		matchesTwo.setLosingTeamScore(4);
+		matchesTwo.setMatchDate("2021-01-01");
+		matchesTwo.setMatchTime("12:00");
+		
+		matchesThree.setMatchId(3000);
+		matchesThree.setTournamentId(3);
+		matchesThree.setTeamOneId(3);
+		matchesThree.setTeamTwoId(4);
+		matchesThree.setWinningTeamId(3);
+		matchesThree.setLosingTeamId(4);
+		matchesThree.setWinningTeamScore(5);
+		matchesThree.setLosingTeamScore(4);
+		matchesThree.setMatchDate("2021-01-01");
+		matchesThree.setMatchTime("12:00");
+	}
+	//getAllMatches
+	//getMatchById
+	//createMatch
+	//updateMatch
+	//deleteMatch
+	
+	@Test
+	public void getAllMatches_returns_all_matches() {
+		//Arrange
+		
+		
+		//Act
+		
+		
+		//Assert
+		
+	}
+	
+	@Test
+	public void getMatchById_returns_the_correct_match() {
+		//Arrange
+		
+		
+		//Act
+		
+		
+		//Assert
+		
+	}
+	
+	@Test
+	public void createMatch_successfully_creates_a_match() {
+		//Arrange
+		
+		
+		//Act
+		
+		
+		//Assert
+		
+	}
+	
+	@Test
+	public void updateMatch_successfully_updates_a_match() {
+		//Arrange
+		
+		
+		//Act
+		
+		
+		//Assert
+		
+	}
+	
+	@Test
+	public void deleteMatch_deletes_a_match() {
+		//Arrange
+		
+		
+		//Act
+		
+		
+		//Assert
+		
+	}
+}
